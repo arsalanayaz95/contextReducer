21bf3b7678ca9b53e8ce3f350e77b783794a944f
Arsalan Ayaz <arsalan.ayaz95@gmail.com>
2020-06-16 20:15:03 +0500

    build commit

---
+++ build/precache-manifest.512ebaeea9dedd5e06b6a7e623c4f2f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49dd2add3903cf88e78a4d090109a44c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4446c26b63be320c1726",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a9d560e72a2250324f81",
+    "url": "/static/js/2.7729dfe8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7729dfe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4446c26b63be320c1726",
+    "url": "/static/js/main.ffeba324.chunk.js"
+  },
+  {
+    "revision": "fb624750aeb5eb1a0f32",
+    "url": "/static/js/runtime-main.95b99f3c.js"
+  }
+]);
